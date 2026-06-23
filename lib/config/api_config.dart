8500fce0da544bbc94cbd5102ea8198a618eb700
yong geon <gunnylove@gmail.com>
2026-06-23 18:12:11 +0900

    update

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 abstract final class ApiConfig {
   static const profileApiBaseUrl = String.fromEnvironment(
     'PROFILE_API_BASE_URL',
-    defaultValue: 'http://localhost:3000',
+    defaultValue: 'https://tabata-server.vercel.app',
   );
 }
